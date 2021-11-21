35385f0fd68316b36a726ce9c3c12cdd280ae82a
Acceptable67 <31579132+Acceptable67@users.noreply.github.com>
2021-11-21 17:29:56 -0500

    Made LibSprite a library (DFS doesn't work) + added to CPPLibDragonN64.

--- CPPLibDragonN64/main.cpp
+++ CPPLibDragonN64/main.cpp
@@ -1,9 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
-
-#include <SVector.h>
+#include <LibSprite.h>
 #include "LibN64.h"
-
+#include "Sprites.h"
 /*  virtual functions to override:
     KeyAPressed
     KeyBPressed
@@ -11,16 +10,19 @@
     KeyDDPressed
     KeyDLPressed
     KeyDRPressed
+    KeyZPressed
     OnCreate
     FrameUpdate
 */
 
+libSprite logo;
 class Instance : public LibN64 {
 public:
     Instance(resolution_t res, bitdepth_t dep) : LibN64(res, dep) {}
 
     virtual void OnCreate() 
     {
+        logo.Ready(0x50, 0x4E, n64logo);
         romTitle = "LibN64 Test";
     }
     
@@ -30,6 +32,9 @@ protected:
         DrawText(5, 10, "This is a test of the CPP LibDragon\nframework.");
         DrawCircle(30, 60, 6, RED);
         DrawTextFormat<float, float>(30, 90, (char*)"Total %f Elapsed %f", TICKS2SECONDS(fTotalTime), TICKS2SECONDS(fFrameTime));
+
+        /*Draw n64 logo*/
+        logo.Draw(LibN64_Display, 10, 160, 1, 1);
         
     }
     
--- TestLibProj/LibSprite.cpp
+++ TestLibProj/LibSprite.cpp
@@ -1,1 +1,20 @@
 #include "LibSprite.h"
+#include <stdlib.h>
+
+void libSprite::Ready(int w, int h, int* nd) {
+    this->Width = w;
+    this->Height = h;
+    this->Data = (int*)malloc(Width * Height);
+    this->Data = nd;
+}
+
+void libSprite::Draw(display_context_t disp, int x, int y, bool alpha, int scale) {
+    for (int h = 0; h < Height; h++) {
+        for (int w = 0; w < Width; w++) {
+            if (!(Data[h * Width + w] >= 0xFFFE0000 && alpha == 1)) {
+                //graphics_draw_pixel(disp, x + (w), y + (h), sprArray[h * IMAGE_WIDTH + w]);
+                graphics_draw_box(disp, x + (w * scale), y + (h * scale), scale, scale, Data[h * Width + w]);
+            }
+        }
+    }
+}
